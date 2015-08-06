d54afdffc42888709bdc6b03037e1eaf050bf27d
Michele Mantovan <michele.mantovan87@gmail.com>
2015-08-06 19:58:25 +0200

    [py] minor optimization

--- python/easing.py
+++ python/easing.py
@@ -12,80 +12,92 @@ class SinEase():
 
 	@staticmethod
 	def ease_in(curr_time, start_val, end_val, duration):
-		return -(end_val - start_val) * math.cos(curr_time/duration * (math.pi/2.0)) + (end_val - start_val) + start_val
+		change = (end_val - start_val)
+		return -change * math.cos(curr_time/duration * (math.pi*.5)) + change + start_val
 
 	@staticmethod
 	def ease_out(curr_time, start_val, end_val, duration):
-		 return (end_val - start_val) * math.sin(curr_time/duration * (math.pi/2.0)) + start_val
+		change = (end_val - start_val)
+		return change * math.sin(curr_time/duration * (math.pi*.5)) + start_val
 
 	@staticmethod
 	def ease_in_out(curr_time, start_val, end_val, duration):
-		return -(end_val-start_val) * .5 * (math.cos(math.pi*(curr_time/duration)) - 1.0) + start_val
+		change = (end_val - start_val)
+		return -change * .5 * (math.cos(math.pi*(curr_time/duration)) - 1.0) + start_val
 
 
 class QuadEase():
 
 	@staticmethod
 	def ease_in(curr_time, start_val, end_val, duration):
+		change = (end_val - start_val)
 		curr_time /= duration;
-		return (end_val - start_val)* curr_time*curr_time + start_val
+		return change * curr_time**2 + start_val
 
 	@staticmethod
 	def ease_out(curr_time, start_val, end_val, duration):
+		change = (end_val - start_val)
 		curr_time /= duration;
-		return -(end_val - start_val) * curr_time*(curr_time-2.0) + start_val
+		return -change * curr_time * (curr_time - 2.0) + start_val
 
 	@staticmethod
 	def ease_in_out(curr_time, start_val, end_val, duration):
 		curr_time /= (duration * .5)
+		change = (end_val - start_val)
 		if curr_time < 1:
-			return (end_val-start_val) * .5 * curr_time * curr_time + start_val
+			return change * .5 * curr_time**2 + start_val
 		curr_time -= 1.0
-		return -(end_val-start_val) * .5 * (curr_time * (curr_time - 2.0) - 1.0) + start_val
+		return -change * .5 * (curr_time * (curr_time - 2.0) - 1.0) + start_val
 
 
 class CubicEase():
 
 	@staticmethod
 	def ease_in(curr_time, start_val, end_val, duration):
+		change = (end_val - start_val)
 		curr_time /= duration
-		return (end_val - start_val)*curr_time*curr_time*curr_time + start_val
+		return change * curr_time**3 + start_val
 
 	@staticmethod
 	def ease_out(curr_time, start_val, end_val, duration):
+		change = (end_val - start_val)
 		curr_time /= duration
 		curr_time -= 1.0
-		return (end_val - start_val)*(curr_time*curr_time*curr_time + 1) + start_val
+		return change * (curr_time**3 + 1) + start_val
 
 	@staticmethod
 	def ease_in_out(curr_time, start_val, end_val, duration):
-		curr_time /= (duration*.5)
+		change = (end_val - start_val)
+		curr_time /= duration*.5
 		if curr_time < 1.0:
-			return (end_val - start_val)*.5*curr_time*curr_time*curr_time + start_val
+			return change * .5 * curr_time**3 + start_val
 		curr_time -= 2.0
-		return (end_val - start_val)*.5*(curr_time*curr_time*curr_time + 2.0) + start_val
+		return change * .5 * (curr_time**3 + 2.0) + start_val
 
 
 class QuarticEase():
 
 	@staticmethod
 	def ease_in(curr_time, start_val, end_val, duration):
+		change = (end_val - start_val)
 		curr_time /= duration
-		return (end_val - start_val)*curr_time*curr_time*curr_time*curr_time + start_val
+		return change * curr_time**4 + start_val
 
 	@staticmethod
 	def ease_out(curr_time, start_val, end_val, duration):
+		change = (end_val - start_val)
 		curr_time /= duration
 		curr_time -= 1.0
-		return -(end_val - start_val) * (curr_time*curr_time*curr_time*curr_time - 1.0) + start_val
+		return -change * (curr_time**4 - 1.0) + start_val
 
 	@staticmethod
 	def ease_in_out(curr_time, start_val, end_val, duration):
+		change = (end_val - start_val)
 		curr_time /= duration*.5;
 		if curr_time < 1.0:
-			return (end_val - start_val)*.5*curr_time*curr_time*curr_time*curr_time + start_val
+			return change * .5 * curr_time**4 + start_val
 		curr_time -= 2.0
-		return -(end_val - start_val)*.5 * (curr_time*curr_time*curr_time*curr_time - 2.0) + start_val
+		return -change*.5 * (curr_time**4 - 2.0) + start_val
 
 
 class QuinticEase():
@@ -216,4 +228,4 @@ class BackEase():
 		if curr_time < 1:
 			return (end_val - start_val)*.5*(curr_time*curr_time*((back_dist+1)*curr_time - back_dist)) + start_val
 		curr_time -= 2.0
-		return (end_val - start_val)*.5*(curr_time*curr_time*((back_dist+1.0)*curr_time + back_dist) + 2.0) + start_val
+		return (end_val - start_val)*.5*(curr_time*curr_time*((back_dist+1.0)*curr_time + back_dist) + 2.0) + start_val + start_val
