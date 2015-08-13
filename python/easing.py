d241d62c65c44fba96d0bf03b87425d472e7e379
Michele Mantovan <michele.mantovan87@gmail.com>
2015-08-13 12:13:31 +0200

    pep8 compliance

--- python/easing.py
+++ python/easing.py
@@ -1,239 +1,255 @@
 import math
 
- 
+
 class LinearEase():
 
-	@staticmethod
-	def ease_in_out(curr_time, start_val, end_val, duration):
-		return (end_val-start_val)*(curr_time/duration) + start_val
+    @staticmethod
+    def ease_in_out(curr_time, start_val, end_val, duration):
+        return (end_val-start_val)*(curr_time/duration) + start_val
 
 
 class SinEase():
 
-	@staticmethod
-	def ease_in(curr_time, start_val, end_val, duration):
-		change = end_val - start_val
-		return -change * math.cos(curr_time/duration * (math.pi*.5)) + change + start_val
+    @staticmethod
+    def ease_in(curr_time, start_val, end_val, duration):
+        change = end_val - start_val
+        return -change * math.cos(curr_time/duration * (math.pi*.5))\
+            + change + start_val
 
-	@staticmethod
-	def ease_out(curr_time, start_val, end_val, duration):
-		change = end_val - start_val
-		return change * math.sin(curr_time/duration * (math.pi*.5)) + start_val
+    @staticmethod
+    def ease_out(curr_time, start_val, end_val, duration):
+        change = end_val - start_val
+        return change * math.sin(curr_time/duration * (math.pi*.5)) + start_val
 
-	@staticmethod
-	def ease_in_out(curr_time, start_val, end_val, duration):
-		change = end_val - start_val
-		return -change * .5 * (math.cos(math.pi*(curr_time/duration)) - 1.0) + start_val
+    @staticmethod
+    def ease_in_out(curr_time, start_val, end_val, duration):
+        change = end_val - start_val
+        return -change * .5 * (math.cos(math.pi*(curr_time/duration)) - 1.0)\
+            + start_val
 
 
 class QuadEase():
 
-	@staticmethod
-	def ease_in(curr_time, start_val, end_val, duration):
-		change = end_val - start_val
-		curr_time /= duration;
-		return change * curr_time**2 + start_val
+    @staticmethod
+    def ease_in(curr_time, start_val, end_val, duration):
+        change = end_val - start_val
+        curr_time /= duration
+        return change * curr_time**2 + start_val
 
-	@staticmethod
-	def ease_out(curr_time, start_val, end_val, duration):
-		change = end_val - start_val
-		curr_time /= duration;
-		return -change * curr_time * (curr_time - 2.0) + start_val
+    @staticmethod
+    def ease_out(curr_time, start_val, end_val, duration):
+        change = end_val - start_val
+        curr_time /= duration
+        return -change * curr_time * (curr_time - 2.0) + start_val
 
-	@staticmethod
-	def ease_in_out(curr_time, start_val, end_val, duration):
-		curr_time /= (duration * .5)
-		change = end_val - start_val
-		if curr_time < 1:
-			return change * .5 * curr_time**2 + start_val
-		curr_time -= 1.0
-		return -change * .5 * (curr_time * (curr_time - 2.0) - 1.0) + start_val
+    @staticmethod
+    def ease_in_out(curr_time, start_val, end_val, duration):
+        curr_time /= (duration * .5)
+        change = end_val - start_val
+        if curr_time < 1:
+            return change * .5 * curr_time**2 + start_val
+        curr_time -= 1.0
+        return -change * .5 * (curr_time * (curr_time - 2.0) - 1.0) + start_val
 
 
 class CubicEase():
 
-	@staticmethod
-	def ease_in(curr_time, start_val, end_val, duration):
-		change = end_val - start_val
-		curr_time /= duration
-		return change * curr_time**3 + start_val
+    @staticmethod
+    def ease_in(curr_time, start_val, end_val, duration):
+        change = end_val - start_val
+        curr_time /= duration
+        return change * curr_time**3 + start_val
 
-	@staticmethod
-	def ease_out(curr_time, start_val, end_val, duration):
-		change = end_val - start_val
-		curr_time /= duration
-		curr_time -= 1.0
-		return change * (curr_time**3 + 1) + start_val
+    @staticmethod
+    def ease_out(curr_time, start_val, end_val, duration):
+        change = end_val - start_val
+        curr_time /= duration
+        curr_time -= 1.0
+        return change * (curr_time**3 + 1) + start_val
 
-	@staticmethod
-	def ease_in_out(curr_time, start_val, end_val, duration):
-		change = end_val - start_val
-		curr_time /= duration*.5
-		if curr_time < 1.0:
-			return change * .5 * curr_time**3 + start_val
-		curr_time -= 2.0
-		return change * .5 * (curr_time**3 + 2.0) + start_val
+    @staticmethod
+    def ease_in_out(curr_time, start_val, end_val, duration):
+        change = end_val - start_val
+        curr_time /= duration*.5
+        if curr_time < 1.0:
+            return change * .5 * curr_time**3 + start_val
+        curr_time -= 2.0
+        return change * .5 * (curr_time**3 + 2.0) + start_val
 
 
 class QuarticEase():
 
-	@staticmethod
-	def ease_in(curr_time, start_val, end_val, duration):
-		change = end_val - start_val
-		curr_time /= duration
-		return change * curr_time**4 + start_val
+    @staticmethod
+    def ease_in(curr_time, start_val, end_val, duration):
+        change = end_val - start_val
+        curr_time /= duration
+        return change * curr_time**4 + start_val
 
-	@staticmethod
-	def ease_out(curr_time, start_val, end_val, duration):
-		change = end_val - start_val
-		curr_time /= duration
-		curr_time -= 1.0
-		return -change * (curr_time**4 - 1.0) + start_val
+    @staticmethod
+    def ease_out(curr_time, start_val, end_val, duration):
+        change = end_val - start_val
+        curr_time /= duration
+        curr_time -= 1.0
+        return -change * (curr_time**4 - 1.0) + start_val
 
-	@staticmethod
-	def ease_in_out(curr_time, start_val, end_val, duration):
-		change = end_val - start_val
-		curr_time /= duration * .5
-		if curr_time < 1.0:
-			return change * .5 * curr_time**4 + start_val
-		curr_time -= 2.0
-		return -change * .5 * (curr_time**4 - 2.0) + start_val
+    @staticmethod
+    def ease_in_out(curr_time, start_val, end_val, duration):
+        change = end_val - start_val
+        curr_time /= duration * .5
+        if curr_time < 1.0:
+            return change * .5 * curr_time**4 + start_val
+        curr_time -= 2.0
+        return -change * .5 * (curr_time**4 - 2.0) + start_val
 
 
 class QuinticEase():
 
-	@staticmethod
-	def ease_in(curr_time, start_val, end_val, duration):
-		curr_time /= duration
-		return (end_val - start_val) * curr_time**5 + start_val
+    @staticmethod
+    def ease_in(curr_time, start_val, end_val, duration):
+        curr_time /= duration
+        return (end_val - start_val) * curr_time**5 + start_val
 
-	@staticmethod
-	def ease_out(curr_time, start_val, end_val, duration):
-		curr_time /= duration
-		curr_time -= 1.0
-		return (end_val - start_val) * (curr_time**5 + 1.0) + start_val
+    @staticmethod
+    def ease_out(curr_time, start_val, end_val, duration):
+        curr_time /= duration
+        curr_time -= 1.0
+        return (end_val - start_val) * (curr_time**5 + 1.0) + start_val
 
-	@staticmethod
-	def ease_in_out(curr_time, start_val, end_val, duration):
-		change = end_val - start_val
-		curr_time /= duration * .5
-		if (curr_time < 1.0):
-			return change * .5 * curr_time**5 + start_val
-		curr_time -= 2.0
-		return change* .5 * (curr_time**5 + 2.0) + start_val
+    @staticmethod
+    def ease_in_out(curr_time, start_val, end_val, duration):
+        change = end_val - start_val
+        curr_time /= duration * .5
+        if (curr_time < 1.0):
+            return change * .5 * curr_time**5 + start_val
+        curr_time -= 2.0
+        return change * .5 * (curr_time**5 + 2.0) + start_val
 
 
 class ExpEase():
 
-	@staticmethod
-	def ease_in(curr_time, start_val, end_val, duration):
-		return (end_val - start_val) * math.pow(2, 
-			10.0 * (curr_time/duration - 1.0) ) + start_val
+    @staticmethod
+    def ease_in(curr_time, start_val, end_val, duration):
+        return (end_val - start_val) * \
+            math.pow(2, 10.0 * (curr_time/duration - 1.0)) + start_val
 
-	@staticmethod
-	def ease_out(curr_time, start_val, end_val, duration):
-		return (end_val - start_val) * ( -math.pow(2,
-			-10.0 * curr_time/duration ) + 1.0 ) + start_val
+    @staticmethod
+    def ease_out(curr_time, start_val, end_val, duration):
+        return (end_val - start_val) * \
+            (-math.pow(2, -10.0 * curr_time/duration) + 1.0) + start_val
 
-	@staticmethod
-	def ease_in_out(curr_time, start_val, end_val, duration):
-		change = end_val - start_val
-		curr_time /= duration * .5
-		if curr_time < 1.0:
-			return change * .5 *  math.pow(2, 10 * (curr_time - 1.0) ) + start_val
-		curr_time -= 1.0
-		return change * .5 * ( -math.pow(2, -10.0 * curr_time) + 2.0 ) + start_val
+    @staticmethod
+    def ease_in_out(curr_time, start_val, end_val, duration):
+        change = end_val - start_val
+        curr_time /= duration * .5
+        if curr_time < 1.0:
+            return change * .5 * math.pow(2, 10 * (curr_time - 1.0))\
+                + start_val
+        curr_time -= 1.0
+        return change * .5 * (-math.pow(2, -10.0 * curr_time) + 2.0)\
+            + start_val
 
 
 class CircularEase():
 
-	@staticmethod
-	def ease_in(curr_time, start_val, end_val, duration):
-		curr_time /= duration
-		return -(end_val - start_val) * (math.sqrt(1 - curr_time**2) - 1) + start_val
+    @staticmethod
+    def ease_in(curr_time, start_val, end_val, duration):
+        curr_time /= duration
+        return -(end_val - start_val) * (math.sqrt(1 - curr_time**2) - 1)\
+            + start_val
 
-	@staticmethod
-	def ease_out(curr_time, start_val, end_val, duration):
-		curr_time /= duration
-		curr_time -= 1.0;
-		return (end_val - start_val) * math.sqrt(1 - curr_time**2) + start_val
+    @staticmethod
+    def ease_out(curr_time, start_val, end_val, duration):
+        curr_time /= duration
+        curr_time -= 1.0
+        return (end_val - start_val) * math.sqrt(1 - curr_time**2) + start_val
 
-	@staticmethod
-	def ease_in_out(curr_time, start_val, end_val, duration):
-		change = end_val - start_val
-		curr_time /= duration * .5
-		if curr_time < 1:
-			return -change * .5 * (math.sqrt(1 - curr_time**2) - 1.0) + start_val
-		curr_time -= 2.0
-		return change * .5 * (math.sqrt(1 - curr_time**2) + 1.0) + start_val
+    @staticmethod
+    def ease_in_out(curr_time, start_val, end_val, duration):
+        change = end_val - start_val
+        curr_time /= duration * .5
+        if curr_time < 1:
+            return -change * .5 * (math.sqrt(1 - curr_time**2) - 1.0)\
+                + start_val
+        curr_time -= 2.0
+        return change * .5 * (math.sqrt(1 - curr_time**2) + 1.0)\
+            + start_val
 
 
 class ElasticEase():
 
-	@staticmethod
-	def ease_in(curr_time, start_val, end_val, duration, k=.3):
-		if curr_time == 0: 
-			return start_val
-		curr_time/=duration
-		if curr_time == 1.0:
-			return end_val
-		p = duration* k
-		change = end_val - start_val
-		s = p/4.0
-		curr_time-=1
-		postFix = change * math.pow(2, 10 * curr_time)
-		return -(postFix * math.sin((curr_time * duration - s) * (2 * math.pi)/p)) + start_val
+    @staticmethod
+    def ease_in(curr_time, start_val, end_val, duration, k=.3):
+        if curr_time == 0:
+            return start_val
+        curr_time /= duration
+        if curr_time == 1.0:
+            return end_val
+        p = duration * k
+        change = end_val - start_val
+        s = p/4.0
+        curr_time -= 1
+        postFix = change * math.pow(2, 10 * curr_time)
+        return -(postFix * math.sin(
+            (curr_time * duration - s) * (2 * math.pi)/p)) + start_val
 
-	@staticmethod
-	def ease_out(curr_time, start_val, end_val, duration, k=.3, b=2):
-		change = end_val-start_val
-	 	curr_time /= duration
-	 	if curr_time == 1:
-	 		return end_val
-	 	p = duration * k
-	 	s = p/4.0
-	 	return change * math.pow(b, -10.0 * curr_time) * \
-	 		math.sin((curr_time * duration - s)\
-	 			* (2.0 * math.pi)/p) + change + start_val
+    @staticmethod
+    def ease_out(curr_time, start_val, end_val, duration, k=.3, b=2):
+        change = end_val-start_val
+        curr_time /= duration
+        if curr_time == 1:
+            return end_val
+        p = duration * k
+        s = p/4.0
+        return change * math.pow(b, -10.0 * curr_time)\
+            * math.sin((curr_time * duration - s) * (2.0 * math.pi)/p)\
+            + change + start_val
 
-
-	@staticmethod
-	def ease_in_out(curr_time, start_val, end_val, duration, k=.3):
-		if curr_time==0: 
-			return start_val
-		curr_time /= duration/2.0
-		if curr_time == 2.0:
-			return end_val
-		p = duration * k * 1.5
-		change = (end_val - start_val)
-		s = p/4.0
-		if curr_time < 1:
-			curr_time-=1
-			postFix = change*math.pow(2, 10*(curr_time))
-			return -.5*(postFix * math.sin((curr_time*duration-s)*(2.0 * math.pi)/p )) + start_val 
-		curr_time-=1
-		postFix = change * math.pow(2, -10*(curr_time))
-		return postFix * math.sin((curr_time*duration-s)*(2.0 * math.pi)/p)*.5  + change + start_val
-
+    @staticmethod
+    def ease_in_out(curr_time, start_val, end_val, duration, k=.3):
+        if curr_time == 0:
+            return start_val
+        curr_time /= duration/2.0
+        if curr_time == 2.0:
+            return end_val
+        p = duration * k * 1.5
+        change = (end_val - start_val)
+        s = p/4.0
+        if curr_time < 1:
+            curr_time -= 1
+            postFix = change*math.pow(2, 10*(curr_time))
+            return -.5 * \
+                (postFix * math.sin((curr_time*duration-s)*(2.0 * math.pi)/p))\
+                + start_val
+        curr_time -= 1
+        postFix = change * math.pow(2, -10*(curr_time))
+        return postFix * math.sin((curr_time*duration-s)*(2.0 * math.pi)/p)\
+            * .5 + change + start_val
 
 
 class BackEase():
 
-	@staticmethod
-	def ease_in(curr_time, start_val, end_val, duration, back_dist=1.7):
-		postFix = curr_time/duration
-		return (end_val - start_val)*postFix*curr_time*((back_dist+1)*curr_time - back_dist) + start_val
+    @staticmethod
+    def ease_in(curr_time, start_val, end_val, duration, back_dist=1.7):
+        postFix = curr_time/duration
+        return (end_val - start_val) * postFix*curr_time * \
+            ((back_dist+1)*curr_time - back_dist) + start_val
 
-	@staticmethod
-	def ease_out(curr_time, start_val, end_val, duration, back_dist=1.7):
-		curr_time=curr_time/duration-1
-		return (end_val - start_val)*(curr_time*curr_time*((back_dist+1)*curr_time + back_dist) + 1) + start_val;
+    @staticmethod
+    def ease_out(curr_time, start_val, end_val, duration, back_dist=1.7):
+        curr_time = curr_time/duration-1
+        return (end_val - start_val) * \
+            (curr_time*curr_time*((back_dist+1)*curr_time + back_dist) + 1)\
+            + start_val
 
-	@staticmethod
-	def ease_in_out(curr_time, start_val, end_val, duration, back_dist=1.7):
-		back_dist*=1.525
-		curr_time/=duration*.5
-		if curr_time < 1:
-			return (end_val - start_val)*.5*(curr_time*curr_time*((back_dist+1)*curr_time - back_dist)) + start_val
-		curr_time -= 2.0
-		return (end_val - start_val)*.5*(curr_time*curr_time*((back_dist+1.0)*curr_time + back_dist) + 2.0) + start_val + start_val
+    @staticmethod
+    def ease_in_out(curr_time, start_val, end_val, duration, back_dist=1.7):
+        back_dist *= 1.525
+        curr_time /= duration*.5
+        if curr_time < 1:
+            return (end_val - start_val) * .5 * \
+                (curr_time*curr_time*((back_dist+1.0)*curr_time - back_dist))\
+                + start_val
+        curr_time -= 2.0
+        return (end_val - start_val) * .5 * \
+            (curr_time*curr_time*((back_dist+1.0)*curr_time+back_dist) + 2.0)\
+            + start_val
--- python/panda_test_tool.py
+++ python/panda_test_tool.py
@@ -5,143 +5,147 @@ from panda3d.core import *
 from easing import *
 import sys
 
-easeClass = ['lin', 'sin', 'quad', 'cub', 'quar', 'quin', 'exp', 'circ', 'elas', 'back']
+easeClass = ['lin', 'sin', 'quad', 'cub', 'quar',
+             'quin', 'exp', 'circ', 'elas', 'back']
 easeType = ['easeI', 'easeO', 'easeIO']
 easeParam = ['pos', 'scale']
 
+
 class MyApp(ShowBase):
 
-	ids2Ease = {('lin', 'easeI'): LinearEase.ease_in_out,
-			   ('lin', 'easeO'): LinearEase.ease_in_out,
-		   	   ('lin', 'easeIO'): LinearEase.ease_in_out,
-			   ('sin', 'easeI'): SinEase.ease_in,
-			   ('sin', 'easeO'): SinEase.ease_out,
-			   ('sin', 'easeIO'): SinEase.ease_in_out,
-			   ('quad', 'easeI'): QuadEase.ease_in,
-			   ('quad', 'easeO'): QuadEase.ease_out,
-			   ('quad', 'easeIO'): QuadEase.ease_in_out,
-			   ('cub', 'easeI'): CubicEase.ease_in,
-			   ('cub', 'easeO'): CubicEase.ease_out,
-			   ('cub', 'easeIO'): CubicEase.ease_in_out,
-			   ('quar', 'easeI'): QuarticEase.ease_in,
-			   ('quar', 'easeO'): QuarticEase.ease_out,
-			   ('quar', 'easeIO'): QuarticEase.ease_in_out,
-			   ('quin', 'easeI'): QuinticEase.ease_in,
-			   ('quin', 'easeO'): QuinticEase.ease_out,
-			   ('quin', 'easeIO'): QuinticEase.ease_in_out,
-			   ('exp', 'easeI'): ExpEase.ease_in,
-			   ('exp', 'easeO'): ExpEase.ease_out,
-			   ('exp', 'easeIO'): ExpEase.ease_in_out,
-			   ('circ', 'easeI'): CircularEase.ease_in,
-			   ('circ', 'easeO'): CircularEase.ease_out,
-			   ('circ', 'easeIO'): CircularEase.ease_in_out,
-			   ('elas', 'easeI'): ElasticEase.ease_in,
-			   ('elas', 'easeO'): ElasticEase.ease_out,
-			   ('elas', 'easeIO'): ElasticEase.ease_in_out,
-			   ('back', 'easeI'): BackEase.ease_in,
-			   ('back', 'easeO'): BackEase.ease_out,
-			   ('back', 'easeIO'): BackEase.ease_in_out,
-			   }
+    ids2Ease = {('lin', 'easeI'): LinearEase.ease_in_out,
+                ('lin', 'easeO'): LinearEase.ease_in_out,
+                ('lin', 'easeIO'): LinearEase.ease_in_out,
+                ('sin', 'easeI'): SinEase.ease_in,
+                ('sin', 'easeO'): SinEase.ease_out,
+                ('sin', 'easeIO'): SinEase.ease_in_out,
+                ('quad', 'easeI'): QuadEase.ease_in,
+                ('quad', 'easeO'): QuadEase.ease_out,
+                ('quad', 'easeIO'): QuadEase.ease_in_out,
+                ('cub', 'easeI'): CubicEase.ease_in,
+                ('cub', 'easeO'): CubicEase.ease_out,
+                ('cub', 'easeIO'): CubicEase.ease_in_out,
+                ('quar', 'easeI'): QuarticEase.ease_in,
+                ('quar', 'easeO'): QuarticEase.ease_out,
+                ('quar', 'easeIO'): QuarticEase.ease_in_out,
+                ('quin', 'easeI'): QuinticEase.ease_in,
+                ('quin', 'easeO'): QuinticEase.ease_out,
+                ('quin', 'easeIO'): QuinticEase.ease_in_out,
+                ('exp', 'easeI'): ExpEase.ease_in,
+                ('exp', 'easeO'): ExpEase.ease_out,
+                ('exp', 'easeIO'): ExpEase.ease_in_out,
+                ('circ', 'easeI'): CircularEase.ease_in,
+                ('circ', 'easeO'): CircularEase.ease_out,
+                ('circ', 'easeIO'): CircularEase.ease_in_out,
+                ('elas', 'easeI'): ElasticEase.ease_in,
+                ('elas', 'easeO'): ElasticEase.ease_out,
+                ('elas', 'easeIO'): ElasticEase.ease_in_out,
+                ('back', 'easeI'): BackEase.ease_in,
+                ('back', 'easeO'): BackEase.ease_out,
+                ('back', 'easeIO'): BackEase.ease_in_out,
+                }
 
-	def __init__(self):
-		ShowBase.__init__(self)
-		self.curr_ease = ['elas', 'easeO']
-		self.curr_param = 'pos'
-		self.class_lab = OnscreenText(text=self.curr_ease[0],
-								  pos=(-1.2, .9), 
-								  scale=0.07,
-								  fg=(1, 0.8, 0.8, 1),
-								  align=TextNode.ALeft,
-								  mayChange=1)
+    def __init__(self):
+        ShowBase.__init__(self)
+        self.curr_ease = ['elas', 'easeO']
+        self.curr_param = 'pos'
+        self.class_lab = OnscreenText(text=self.curr_ease[0],
+                                      pos=(-1.2, .9),
+                                      scale=0.07,
+                                      fg=(1, 0.8, 0.8, 1),
+                                      align=TextNode.ALeft,
+                                      mayChange=1)
 
-		self.type_lab = OnscreenText(text=self.curr_ease[1],
-								  pos=(-1.0, .9), 
-								  scale=0.07,
-								  fg=(1, 0.8, 0.8, 1),
-								  align=TextNode.ALeft,
-								  mayChange=1)
+        self.type_lab = OnscreenText(text=self.curr_ease[1],
+                                     pos=(-1.0, .9),
+                                     scale=0.07,
+                                     fg=(1, 0.8, 0.8, 1),
+                                     align=TextNode.ALeft,
+                                     mayChange=1)
 
-		self.param_lab = OnscreenText(text=self.curr_param,
-								  pos=(-.8, .9), 
-								  scale=0.07,
-								  fg=(1, 0.8, 0.8, 1),
-								  align=TextNode.ALeft,
-								  mayChange=1)
+        self.param_lab = OnscreenText(text=self.curr_param,
+                                      pos=(-.8, .9),
+                                      scale=0.07,
+                                      fg=(1, 0.8, 0.8, 1),
+                                      align=TextNode.ALeft,
+                                      mayChange=1)
 
-		self.positions = [(-.7, 0, 0), (.7, 0, 0)]
-		self.scales = [1, 2]
+        self.positions = [(-.7, 0, 0), (.7, 0, 0)]
+        self.scales = [1, 2]
 
-		self.myFrame = DirectFrame(frameColor=(0, 0, 1, 1),
-                      		  	   frameSize=(-.3, .3, -.3, .3),
-                      		       pos=self.positions[0])
+        self.myFrame = DirectFrame(frameColor=(0, 0, 1, 1),
+                                   frameSize=(-.3, .3, -.3, .3),
+                                   pos=self.positions[0])
 
-		self.curr_time = 0.0
-		self.end_time = 1.0
-		self.accept('q-up', self.__change_class, extraArgs=[1])
-		self.accept('a-up', self.__change_class, extraArgs=[-1])
-		self.accept('w-up', self.__change_type, extraArgs=[1])
-		self.accept('s-up', self.__change_type, extraArgs=[-1])
-		self.accept('e-up', self.__change_param, extraArgs=[1])
-		self.accept('d-up', self.__change_param, extraArgs=[-1])
+        self.curr_time = 0.0
+        self.end_time = 1.0
+        self.accept('q-up', self.__change_class, extraArgs=[1])
+        self.accept('a-up', self.__change_class, extraArgs=[-1])
+        self.accept('w-up', self.__change_type, extraArgs=[1])
+        self.accept('s-up', self.__change_type, extraArgs=[-1])
+        self.accept('e-up', self.__change_param, extraArgs=[1])
+        self.accept('d-up', self.__change_param, extraArgs=[-1])
 
-		self.accept('space-up', self.start_ease)
-		self.accept('escape', sys.exit, [0])
+        self.accept('space-up', self.start_ease)
+        self.accept('escape', sys.exit, [0])
 
-	def start_ease(self):
-		self.ignore('space-up')
-		if self.curr_param == 'pos':
-			taskMgr.add(self.ease_pos_task, 'ease_task')
-		elif self.curr_param == 'scale':
-			taskMgr.add(self.ease_scale_task, 'ease_task')
+    def start_ease(self):
+        self.ignore('space-up')
+        if self.curr_param == 'pos':
+            taskMgr.add(self.ease_pos_task, 'ease_task')
+        elif self.curr_param == 'scale':
+            taskMgr.add(self.ease_scale_task, 'ease_task')
 
-	def ease_pos_task(self, task):
-		if self.curr_time < self.end_time:
-			dt = globalClock.getDt()
-			val = self.ids2Ease[(self.curr_ease[0],
-								 self.curr_ease[1])](self.curr_time,
-													 self.positions[0][0],
-													 self.positions[1][0],
-													 self.end_time)
-			self.curr_time += dt
-			self.myFrame.setPos(val, 0, 0)
-			return task.cont
-		self.curr_time = 0.0
-		self.end_time = 1.0
-		self.myFrame.setPos(self.positions[1])
-		self.positions = self.positions[::-1]
-		self.accept('space-up', self.start_ease)
-		return task.done
+    def ease_pos_task(self, task):
+        if self.curr_time < self.end_time:
+            dt = globalClock.getDt()
+            val = self.ids2Ease[(self.curr_ease[0],
+                                 self.curr_ease[1])](self.curr_time,
+                                                     self.positions[0][0],
+                                                     self.positions[1][0],
+                                                     self.end_time)
+            self.curr_time += dt
+            self.myFrame.setPos(val, 0, 0)
+            return task.cont
+        self.curr_time = 0.0
+        self.end_time = 1.0
+        self.myFrame.setPos(self.positions[1])
+        self.positions = self.positions[::-1]
+        self.accept('space-up', self.start_ease)
+        return task.done
 
-	def ease_scale_task(self, task):
-		if self.curr_time < self.end_time:
-			dt = globalClock.getDt()
-			val = self.ids2Ease[(self.curr_ease[0],
-								 self.curr_ease[1])](self.curr_time,
-													 self.scales[0],
-													 self.scales[1],
-													 self.end_time)
-			self.curr_time += dt
-			self.myFrame.setScale(val)
-			return task.cont
-		self.curr_time = 0.0
-		self.end_time = 1.0
-		self.myFrame.setScale(self.scales[1])
-		self.scales = self.scales[::-1]
-		self.accept('space-up', self.start_ease)
-		return task.done
+    def ease_scale_task(self, task):
+        if self.curr_time < self.end_time:
+            dt = globalClock.getDt()
+            val = self.ids2Ease[(self.curr_ease[0],
+                                 self.curr_ease[1])](self.curr_time,
+                                                     self.scales[0],
+                                                     self.scales[1],
+                                                     self.end_time)
+            self.curr_time += dt
+            self.myFrame.setScale(val)
+            return task.cont
+        self.curr_time = 0.0
+        self.end_time = 1.0
+        self.myFrame.setScale(self.scales[1])
+        self.scales = self.scales[::-1]
+        self.accept('space-up', self.start_ease)
+        return task.done
 
+    def __change_class(self, incr):
+        self.curr_ease[0] = easeClass[
+            (easeClass.index(self.curr_ease[0]) + incr) % len(easeClass)]
+        self.class_lab.setText(self.curr_ease[0])
 
-	def __change_class(self, incr):
-		self.curr_ease[0] = easeClass[(easeClass.index(self.curr_ease[0]) + incr) % len(easeClass)]
-		self.class_lab.setText(self.curr_ease[0]);
+    def __change_type(self, incr):
+        self.curr_ease[1] = easeType[
+            (easeType.index(self.curr_ease[1]) + incr) % len(easeType)]
+        self.type_lab.setText(self.curr_ease[1])
 
-	def __change_type(self, incr):
-		self.curr_ease[1] = easeType[(easeType.index(self.curr_ease[1]) + incr) % len(easeType)]
-		self.type_lab.setText(self.curr_ease[1]);
-
-	def __change_param(self, incr):
-		self.curr_param = easeParam[(easeParam.index(self.curr_param) + incr) % len(easeParam)]
-		self.param_lab.setText(self.curr_param);
+    def __change_param(self, incr):
+        self.curr_param = easeParam[
+            (easeParam.index(self.curr_param) + incr) % len(easeParam)]
+        self.param_lab.setText(self.curr_param)
 
 
 app = MyApp()
