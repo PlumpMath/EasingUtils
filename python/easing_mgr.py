135c8effadb48d7ba16ecfb0c5b0e71ef1470622
Michele Mantovan <michele.mantovan87@gmail.com>
2015-10-06 07:53:07 +0200

    new transition handling

--- python/easing_mgr.py
+++ python/easing_mgr.py
@@ -1,26 +1,41 @@
-from easing import *
+from transition import *
+
+"""EasingMgr
+This class rapresent the connection between the algorithminc
+calculations and the Panda3D engine.
+"""
 
 
 class EasingMgr:
 
-    def __init__():
+    def __init__(self):
         self.__transitions = []
 
-    def add_transition(self, nodepath,
-                       t_class, t_types,
-                       duration, values):
-        transition = Transition.factory(nodepath,
-                                        t_class,
-                                        t_types,
-                                        duration)
+    def add_transition(self, nodepath, t_param, ease_types,
+                            duration, values, cb_done):
+        transition = self.__transition_factory(t_param)(nodepath,
+                                                        t_param,
+                                                        ease_types,
+                                                        duration,
+                                                        values,
+                                                        cb_done)
         self.__transitions.append(transition)
         return transition
 
-    def start_transition(transition):
+    def __transition_factory(self, t_param):
+        if '1D' in t_param:
+            return Transition1D
+        elif '2D' in t_param:
+            return Transition2D
+        elif '3D' in t_param:
+            return Transition3D
+
+
+    def start_transition(self, transition):
         if transition in self.__transitions:
             if not transition.is_updating:
                 taskMgr.add(transition.update, str(transition))
 
-    def remove_transition(transition):
+    def remove_transition(self, transition):
         if transition in self.__transitions:
             self.__transitions.pop(transition)
--- python/transition.py
+++ python/transition.py
@@ -1,84 +1,201 @@
+from easing import *
 
 
 class Transition:
-    __ids2Ease = {('lin', 'easeI'): LinearEase.ease_in_out,
-                  ('lin', 'easeO'): LinearEase.ease_in_out,
-                  ('lin', 'easeIO'): LinearEase.ease_in_out,
-                  ('sin', 'easeI'): SinEase.ease_in,
-                  ('sin', 'easeO'): SinEase.ease_out,
-                  ('sin', 'easeIO'): SinEase.ease_in_out,
-                  ('quad', 'easeI'): QuadEase.ease_in,
-                  ('quad', 'easeO'): QuadEase.ease_out,
+
+    ease_params = ['position3D', 'position2D', 'scale1D', 'scale3D']
+    ease_class = ['lin', 'sin', 'quad', 'cub', 'quar',
+                 'quin', 'exp', 'circ', 'elas', 'back']
+    easing_types = ['easeI', 'easeO', 'easeIO']
+
+    __ids2Ease = {('lin', 'easeI'):   LinearEase.ease_in_out,
+                  ('lin', 'easeO'):   LinearEase.ease_in_out,
+                  ('lin', 'easeIO'):  LinearEase.ease_in_out,
+                  ('sin', 'easeI'):   SinEase.ease_in,
+                  ('sin', 'easeO'):   SinEase.ease_out,
+                  ('sin', 'easeIO'):  SinEase.ease_in_out,
+                  ('quad', 'easeI'):  QuadEase.ease_in,
+                  ('quad', 'easeO'):  QuadEase.ease_out,
                   ('quad', 'easeIO'): QuadEase.ease_in_out,
-                  ('cub', 'easeI'): CubicEase.ease_in,
-                  ('cub', 'easeO'): CubicEase.ease_out,
-                  ('cub', 'easeIO'): CubicEase.ease_in_out,
-                  ('quar', 'easeI'): QuarticEase.ease_in,
-                  ('quar', 'easeO'): QuarticEase.ease_out,
+                  ('cub', 'easeI'):   CubicEase.ease_in,
+                  ('cub', 'easeO'):   CubicEase.ease_out,
+                  ('cub', 'easeIO'):  CubicEase.ease_in_out,
+                  ('quar', 'easeI'):  QuarticEase.ease_in,
+                  ('quar', 'easeO'):  QuarticEase.ease_out,
                   ('quar', 'easeIO'): QuarticEase.ease_in_out,
-                  ('quin', 'easeI'): QuinticEase.ease_in,
-                  ('quin', 'easeO'): QuinticEase.ease_out,
+                  ('quin', 'easeI'):  QuinticEase.ease_in,
+                  ('quin', 'easeO'):  QuinticEase.ease_out,
                   ('quin', 'easeIO'): QuinticEase.ease_in_out,
-                  ('exp', 'easeI'): ExpEase.ease_in,
-                  ('exp', 'easeO'): ExpEase.ease_out,
-                  ('exp', 'easeIO'): ExpEase.ease_in_out,
-                  ('circ', 'easeI'): CircularEase.ease_in,
-                  ('circ', 'easeO'): CircularEase.ease_out,
+                  ('exp', 'easeI'):   ExpEase.ease_in,
+                  ('exp', 'easeO'):   ExpEase.ease_out,
+                  ('exp', 'easeIO'):  ExpEase.ease_in_out,
+                  ('circ', 'easeI'):  CircularEase.ease_in,
+                  ('circ', 'easeO'):  CircularEase.ease_out,
                   ('circ', 'easeIO'): CircularEase.ease_in_out,
-                  ('elas', 'easeI'): ElasticEase.ease_in,
-                  ('elas', 'easeO'): ElasticEase.ease_out,
+                  ('elas', 'easeI'):  ElasticEase.ease_in,
+                  ('elas', 'easeO'):  ElasticEase.ease_out,
                   ('elas', 'easeIO'): ElasticEase.ease_in_out,
-                  ('back', 'easeI'): BackEase.ease_in,
-                  ('back', 'easeO'): BackEase.ease_out,
+                  ('back', 'easeI'):  BackEase.ease_in,
+                  ('back', 'easeO'):  BackEase.ease_out,
                   ('back', 'easeIO'): BackEase.ease_in_out
                   }
 
-    def __init__(self, nodepath, t_class, t_types, duration):
-        self.nodepath = nodepath
-        self.t_class = t_class
-        self.t_types = t_types
-        self.duration = duration
-        self._curr_time = 0
-        self._updating = False
-        self._easing_cbs = [self.__ids2Ease[t_type] for t_type in t_types]
+    def __init__(self, nodepath, t_param, ease_types, duration,
+                 values, cb_done=None):
+        if t_param in Transition.ease_params:
+            self._nodepath = nodepath
+            self._values = values
+            self._curr_time = 0
+            self._duration = duration
+            self._updating = False
+            self._easing_cbs = [self.__ids2Ease[(ease_type)] for ease_type in ease_types]
+            self._panda_func = self._meth_factory(nodepath, t_param)
+            self._cb_done = cb_done
+            self._values
+        else:
+            raise Exception('Ease parameter not valid. Use one from: ' + str(Transition.ease_params))
 
-    def update():
-        if self._curr_time < self.duration:
-            self._updating = True
-            dt = globalClock.getDt()
-            vals = ()
-            for i in range(2):
-                for j in range(len(self._values[0])):
-                    val = self._easing_cbs[i](self.curr_time,
-                                              self.values[i][j],
-                                              self.values[i][j],
-                                              self.end_time)
-                    vals = vals + (val)
-                self.curr_time += dt
-                if len(val) == 1:
-                    self.nodepath.panda_param(vals[0])
-                else:
-                    self.nodepath.panda_param(vals)
-                return task.cont
-            self.curr_time = 0.0
-            self.end_time = 1.0
-            self.nodepath.panda_param(self.values[1])
-            self.values = self.values[::-1]
-            return task.done
+    def update(self, task):
+        raise NotImplementedError()
 
+    @property
     def is_updating(self):
-        return self.__updating
+        return self._updating
 
-    @staticmethod
-    def factory(nodepath, t_param, t_class, t_types, duration):
-        if t_param == 'position3D' and len(t_types) == 3:
-            t_obj = Pos3DTran
-        elif t_param == 'position2D' and len(t_types) == 2:
-            t_obj = Pos2DTran
-        elif t_param == 'scale' and len(t_types) == 1:
-            t_obj = ScaleTran
-        elif t_param == 'scale3D' and len(t_types) == 3:
-            t_obj = Scale3DTran
+    def _meth_factory(self, nodepath, t_param):
+        if 'position' in t_param:
+            t_obj = self._nodepath.setPos
+        elif 'scale' in t_param:
+            t_obj = self._nodepath.setScale
         else:
             return None
-        return t_obj(nodepath, t_class, t_types, duration)
+        return t_obj
+
+    def _right_transition(self, dimension, t_param):
+        if dimension == '1D'  and not dimension in t_param:
+            raise Exception('Wrong transition type, use Transition1D.')
+        elif dimension == '2D' and not dimension in t_param:
+            raise Exception('Wrong transition type, use Transition2D.')
+        elif dimension == '3D'  and not dimension in t_param:
+            raise Exception('Wrong transition type, use Transition3D.')
+
+    def _right_class(self, t_class):
+        if t_class not in Transition.ease_class:
+            raise Exception('Ease class not valid. Use one from: ' + str(Transition.ease_class))
+
+    def _right_type(self, t_type):
+        if t_type not in Transition.easing_types:
+            raise Exception('Ease type not valid. Use one from: ' + str(Transition.easing_types))
+
+    def _param_validation(self, dimension, values, ease_types, t_param):
+        self._right_transition(dimension, t_param)
+
+        # if len(values[0]) != len(ease_types):
+        #     raise Exception('Value dimension different from easing types count.')
+        
+        # if dimension == '1D' and len(ease_types) != 1:
+        #     raise Exception('Transition1D needs only one easing type. You gave ' + str(len(ease_types)))
+        # elif dimension == '2D' and len(ease_types) != 2:
+        #     raise Exception('Transition2D needs two easing types. You gave ' + str(len(ease_types)))
+        # elif dimension == '3D' and len(ease_types) != 3:
+        #     raise Exception('Transition1D needs three easing types. You gave ' + str(len(ease_types)))
+
+        for ease_type in ease_types:
+            self._right_class(ease_type[0])
+            self._right_type(ease_type[1])
+
+
+class Transition1D(Transition):
+
+    def __init__(self, nodepath, t_param, ease_types=[('quad', 'easeIO')],
+                 duration=1.0, values=[[0], [1]], cb_done=None):
+        Transition.__init__(self, nodepath, t_param, ease_types,
+                            duration, values, cb_done)
+        self._param_validation('1D', values, ease_types, t_param)
+
+
+    def update(self, task):
+        if self._curr_time < self._duration:
+            self._updating = True
+            val = None
+            val = self._easing_cbs[0](self._curr_time,
+                                      self._values[0][0],
+                                      self._values[1][0],
+                                      self._duration)
+            dt = globalClock.getDt()
+            self._curr_time += dt
+            self._panda_func(val)
+            return task.cont
+
+        self._curr_time = 0.0
+        self._updating = False
+        self._panda_func(self._values[1][0])
+        self._values = self._values[::-1]
+        if self._cb_done:
+            self._cb_done()
+        return task.done
+
+
+class Transition2D(Transition):
+    def __init__(self, nodepath, t_param, ease_types=[('quad', 'easeIO'),
+                                                      ('quad', 'easeIO')],
+                 duration=1.0, values=[(0, 0), (1, 1)], cb_done=None):
+        Transition.__init__(self, nodepath, t_param, ease_types,
+                            duration, values, cb_done)
+        self._param_validation('2D', values, ease_types, t_param)
+
+    def update(self, task):
+        if self._curr_time < self._duration:
+            self._updating = True
+            vals = ()
+            for i in range(len(self._easing_cbs)):
+                val = self._easing_cbs[i](self._curr_time,
+                                          self._values[0][i],
+                                          self._values[1][i],
+                                          self._duration)
+                vals = tuple(list(vals) + [val])
+            dt = globalClock.getDt()
+            self._curr_time += dt
+            self._panda_func(vals[0], vals[1])
+            return task.cont
+        self._curr_time = 0.0
+        self._updating = False
+        self._panda_func(self._values[1][0], self._values[1][1])
+        self._values = self._values[::-1]
+        if self._cb_done:
+            self._cb_done()
+        return task.done
+
+
+class Transition3D(Transition):
+    def __init__(self, nodepath, t_param, ease_types=[('quad', 'easeIO'),
+                                                      ('quad', 'easeIO'),
+                                                      ('quad', 'easeIO')],
+                 duration=1.0, values=[(0, 0, 0), (1, 1, 1)], cb_done=None):
+        Transition.__init__(self, nodepath, t_param, ease_types,
+                            duration, values, cb_done)
+        self._param_validation('3D', values, ease_types, t_param)
+
+    def update(self, task):
+        if self._curr_time < self._duration:
+            self._updating = True
+            vals = ()
+            for i in range(len(self._easing_cbs)):
+                val = self._easing_cbs[i](self._curr_time,
+                                          self._values[0][i],
+                                          self._values[1][i],
+                                          self._duration)
+                vals = tuple(list(vals) + [val])
+            dt = globalClock.getDt()
+            self._curr_time += dt
+            self._panda_func(vals[0], vals[1], vals[2])
+            return task.cont
+        self._curr_time = 0.0
+        self._updating = False
+        self._panda_func(self._values[1][0],
+                         self._values[1][1],
+                         self._values[1][2])
+        self._values = self._values[::-1]
+        if self._cb_done:
+            self._cb_done()
+        return task.done
