e2798640e12df4cc4780ff4f40a36963ffb30bd8
Michele Mantovan <michele.mantovan87@gmail.com>
2015-10-08 19:49:21 +0200

    new test tool

--- python/panda_test_tool.py
+++ python/panda_test_tool.py
@@ -1,152 +1,194 @@
 from direct.showbase.ShowBase import ShowBase
 from direct.gui.OnscreenText import OnscreenText
 from direct.gui.DirectGui import *
+from easing_mgr import EasingMgr
 from panda3d.core import *
 from easing import *
 import sys
+from direct.gui.OnscreenText import OnscreenText
+
 
 easeClass = ['lin', 'sin', 'quad', 'cub', 'quar',
              'quin', 'exp', 'circ', 'elas', 'back']
 easeType = ['easeI', 'easeO', 'easeIO']
-easeParam = ['pos', 'scale']
+easeParam = ['position3D', 'position2D', 'scale1D', 'scale3D']
+axis = ['1:', '2:', '3:']
 
 
 class MyApp(ShowBase):
-
-    ids2Ease = {('lin', 'easeI'): LinearEase.ease_in_out,
-                ('lin', 'easeO'): LinearEase.ease_in_out,
-                ('lin', 'easeIO'): LinearEase.ease_in_out,
-                ('sin', 'easeI'): SinEase.ease_in,
-                ('sin', 'easeO'): SinEase.ease_out,
-                ('sin', 'easeIO'): SinEase.ease_in_out,
-                ('quad', 'easeI'): QuadEase.ease_in,
-                ('quad', 'easeO'): QuadEase.ease_out,
-                ('quad', 'easeIO'): QuadEase.ease_in_out,
-                ('cub', 'easeI'): CubicEase.ease_in,
-                ('cub', 'easeO'): CubicEase.ease_out,
-                ('cub', 'easeIO'): CubicEase.ease_in_out,
-                ('quar', 'easeI'): QuarticEase.ease_in,
-                ('quar', 'easeO'): QuarticEase.ease_out,
-                ('quar', 'easeIO'): QuarticEase.ease_in_out,
-                ('quin', 'easeI'): QuinticEase.ease_in,
-                ('quin', 'easeO'): QuinticEase.ease_out,
-                ('quin', 'easeIO'): QuinticEase.ease_in_out,
-                ('exp', 'easeI'): ExpEase.ease_in,
-                ('exp', 'easeO'): ExpEase.ease_out,
-                ('exp', 'easeIO'): ExpEase.ease_in_out,
-                ('circ', 'easeI'): CircularEase.ease_in,
-                ('circ', 'easeO'): CircularEase.ease_out,
-                ('circ', 'easeIO'): CircularEase.ease_in_out,
-                ('elas', 'easeI'): ElasticEase.ease_in,
-                ('elas', 'easeO'): ElasticEase.ease_out,
-                ('elas', 'easeIO'): ElasticEase.ease_in_out,
-                ('back', 'easeI'): BackEase.ease_in,
-                ('back', 'easeO'): BackEase.ease_out,
-                ('back', 'easeIO'): BackEase.ease_in_out,
-                }
+    """Test class for the all the uses cases for the EasingMgr class."""
 
     def __init__(self):
         ShowBase.__init__(self)
-        self.curr_ease = ['elas', 'easeO']
-        self.curr_param = 'pos'
-        self.class_lab = OnscreenText(text=self.curr_ease[0],
-                                      pos=(-1.2, .9),
-                                      scale=0.07,
-                                      fg=(1, 0.8, 0.8, 1),
-                                      align=TextNode.ALeft,
-                                      mayChange=1)
 
-        self.type_lab = OnscreenText(text=self.curr_ease[1],
-                                     pos=(-1.0, .9),
-                                     scale=0.07,
-                                     fg=(1, 0.8, 0.8, 1),
-                                     align=TextNode.ALeft,
-                                     mayChange=1)
+        base.disableMouse()
+        base.setBackgroundColor(.2, .2, .2)
+
+        camera.setPos(0, 0, 45)
+        camera.setHpr(0, -90, 0)
+
+        self.curr_ease = [(easeClass[8], easeType[1]),
+                          (easeClass[8], easeType[1]),
+                          (easeClass[8], easeType[1])]
+        self.curr_param = easeParam[2]
+
+        self._labs = []
 
         self.param_lab = OnscreenText(text=self.curr_param,
-                                      pos=(-.8, .9),
+                                      pos=(-1.3, .9),
                                       scale=0.07,
-                                      fg=(1, 0.8, 0.8, 1),
+                                      fg=(0.8, 0.8, 0.8, 1),
                                       align=TextNode.ALeft,
                                       mayChange=1)
 
-        self.positions = [(-.7, 0, 0), (.7, 0, 0)]
-        self.scales = [1, 2]
+        for i in range(3):
+            row = []
+            row.append(OnscreenText(text=axis[i],
+                                    pos=(-.9, .9 - (i*.1)),
+                                    scale=0.07,
+                                    fg=(0.8, 0.8, 0.8, 1),
+                                    align=TextNode.ALeft,
+                                    mayChange=1))
+            row.append(OnscreenText(text=self.curr_ease[i][0],
+                                    pos=(-.8, .9 - (i*.1)),
+                                    scale=0.07,
+                                    fg=(0.8, 0.8, 0.8, 1),
+                                    align=TextNode.ALeft,
+                                    mayChange=1))
 
-        self.myFrame = DirectFrame(frameColor=(0, 0, 1, 1),
-                                   frameSize=(-.3, .3, -.3, .3),
-                                   pos=self.positions[0])
+            row.append(OnscreenText(text=self.curr_ease[i][1],
+                                    pos=(-.6, .9 - (i*.1)),
+                                    scale=0.07,
+                                    fg=(0.8, 0.8, 0.8, 1),
+                                    align=TextNode.ALeft,
+                                    mayChange=1))
+            self._labs.append(row)
 
-        self.curr_time = 0.0
+        self._node = None
+
+        self._ease_values = {'position3D': [(-15.9, 5, -10.7),
+                                            (10.9, -2, 2.9)],
+                             'position2D': [(-.7, -.3), (.7, .3)],
+                             'scale1D': [[.08], [.20]],
+                             'scale3D': [(1, 1, 1), (4, 4, 4)]}
+
         self.end_time = 1.0
-        self.accept('q-up', self.__change_class, extraArgs=[1])
-        self.accept('a-up', self.__change_class, extraArgs=[-1])
-        self.accept('w-up', self.__change_type, extraArgs=[1])
-        self.accept('s-up', self.__change_type, extraArgs=[-1])
-        self.accept('e-up', self.__change_param, extraArgs=[1])
-        self.accept('d-up', self.__change_param, extraArgs=[-1])
 
-        self.accept('space-up', self.start_ease)
         self.accept('escape', sys.exit, [0])
+        self.input_setup(True)
+
+        self.__easingMgr = EasingMgr()
+        self.__curr_tr = None
+
+        self.__change_param(0)
+
+    def input_setup(self, activate):
+        if activate:
+            self.accept('q-up', self.__change_class, extraArgs=[0, 1])
+            self.accept('a-up', self.__change_class, extraArgs=[1, 1])
+            self.accept('z-up', self.__change_class, extraArgs=[2, 1])
+            self.accept('w-up', self.__change_type, extraArgs=[0, 1])
+            self.accept('s-up', self.__change_type, extraArgs=[1, 1])
+            self.accept('x-up', self.__change_type, extraArgs=[2, 1])
+
+            self.accept('p-up', self.__change_param, extraArgs=[1])
+            self.accept('space-up', self.start_ease)
+        else:
+            self.ignore('space-up')
+            self.ignore('q-up')
+            self.ignore('a-up')
+            self.ignore('z-up')
+            self.ignore('w-up')
+            self.ignore('s-up')
+            self.ignore('x-up')
+            self.ignore('p-up')
 
     def start_ease(self):
-        self.ignore('space-up')
-        if self.curr_param == 'pos':
-            taskMgr.add(self.ease_pos_task, 'ease_task')
-        elif self.curr_param == 'scale':
-            taskMgr.add(self.ease_scale_task, 'ease_task')
+        self.input_setup(False)
+        self.__easingMgr.start_transition(self.__curr_tr)
 
-    def ease_pos_task(self, task):
-        if self.curr_time < self.end_time:
-            dt = globalClock.getDt()
-            val = self.ids2Ease[(self.curr_ease[0],
-                                 self.curr_ease[1])](self.curr_time,
-                                                     self.positions[0][0],
-                                                     self.positions[1][0],
-                                                     self.end_time)
-            self.curr_time += dt
-            self.myFrame.setPos(val, 0, 0)
-            return task.cont
-        self.curr_time = 0.0
-        self.end_time = 1.0
-        self.myFrame.setPos(self.positions[1])
-        self.positions = self.positions[::-1]
-        self.accept('space-up', self.start_ease)
-        return task.done
+    def check_finished(self):
+        if not self.__curr_tr.is_updating:
+            self.input_setup(True)
 
-    def ease_scale_task(self, task):
-        if self.curr_time < self.end_time:
-            dt = globalClock.getDt()
-            val = self.ids2Ease[(self.curr_ease[0],
-                                 self.curr_ease[1])](self.curr_time,
-                                                     self.scales[0],
-                                                     self.scales[1],
-                                                     self.end_time)
-            self.curr_time += dt
-            self.myFrame.setScale(val)
-            return task.cont
-        self.curr_time = 0.0
-        self.end_time = 1.0
-        self.myFrame.setScale(self.scales[1])
-        self.scales = self.scales[::-1]
-        self.accept('space-up', self.start_ease)
-        return task.done
+    def __change_class(self, row, incr):
+        self.curr_ease[row] = (easeClass[
+            (easeClass.index(self.curr_ease[row][0]) + incr) % len(easeClass)],
+            self.curr_ease[row][1])
+        self._labs[row][1].setText(self.curr_ease[row][0])
+        self.change_transition()
 
-    def __change_class(self, incr):
-        self.curr_ease[0] = easeClass[
-            (easeClass.index(self.curr_ease[0]) + incr) % len(easeClass)]
-        self.class_lab.setText(self.curr_ease[0])
-
-    def __change_type(self, incr):
-        self.curr_ease[1] = easeType[
-            (easeType.index(self.curr_ease[1]) + incr) % len(easeType)]
-        self.type_lab.setText(self.curr_ease[1])
+    def __change_type(self, row, incr):
+        self.curr_ease[row] = (self.curr_ease[row][0], easeType[
+            (easeType.index(self.curr_ease[row][1]) + incr) % len(easeType)])
+        self._labs[row][2].setText(self.curr_ease[row][1])
+        self.change_transition()
 
     def __change_param(self, incr):
         self.curr_param = easeParam[
             (easeParam.index(self.curr_param) + incr) % len(easeParam)]
         self.param_lab.setText(self.curr_param)
+        self.release_nodes()
+        if self.curr_param in ['position2D', 'scale1D']:
+            self.load_text()
+        if self.curr_param in ['position3D', 'scale3D']:
+            self.load_sphere()
+        if '1D' in self.curr_param:
+            for i, row in enumerate(self._labs):
+                if i < 1:
+                    for lab in row:
+                        lab.show()
+                else:
+                    for lab in row:
+                        lab.hide()
 
+        if '2D' in self.curr_param:
+            for i, row in enumerate(self._labs):
+                if i < 2:
+                    for lab in row:
+                        lab.show()
+                else:
+                    for lab in row:
+                        lab.hide()
+        if '3D' in self.curr_param:
+            for i, row in enumerate(self._labs):
+                for lab in row:
+                    lab.show()
+        self.change_transition()
+
+    def change_transition(self):
+        if self.__curr_tr:
+            self.__easingMgr.remove_transition(self.__curr_tr)
+
+        values = self._ease_values[self.curr_param]
+        self.__curr_tr = self.__easingMgr.add_transition(self._node,
+                                                         self.curr_param,
+                                                         self.curr_ease,
+                                                         self.end_time,
+                                                         values,
+                                                         self.check_finished)
+
+    def load_text(self):
+        self._node = OnscreenText(text='Space bar',
+                                  pos=(self._ease_values['position2D'][0]),
+                                  scale=self._ease_values['scale1D'][0][0])
+
+    def load_sphere(self):
+        self._node = loader.loadModel("planet_sphere")
+        self._node.reparentTo(render)
+        self._node.setScale(self._ease_values['scale3D'][0][0],
+                            self._ease_values['scale3D'][0][1],
+                            self._ease_values['scale3D'][0][2])
+        self._node.setPos(self._ease_values['position3D'][0][0],
+                          self._ease_values['position3D'][0][1],
+                          self._ease_values['position3D'][0][2])
+        self._node_tex = loader.loadTexture("earth.jpg")
+        self._node.setTexture(self._node_tex, 1)
+
+    def release_nodes(self):
+        if self._node:
+            self._node.removeNode()
+            self._node = None
 
 app = MyApp()
 app.run()
